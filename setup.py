81df2384f5b22d06bf8c47820612f565c860b037
Larry Ng <ng.larry@gmail.com>
2012-04-21 03:52:09 -0400

    hotfix v0.2.3a

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='narwal',
     packages=['narwal'],
-    version='0.2.2a',
+    version='0.2.3a',
     author='Larry Ng',
     author_email='ng.larry@gmail.com',
     url='https://github.com/larryng/narwal',
