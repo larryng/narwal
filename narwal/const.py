558013e3265a8bbd8a7874ead51d4dd14ad9e4e9
Larry Ng <ng.larry@gmail.com>
2012-05-08 04:09:49 -0400

    v0.3.0 and commenting out legacy regex

--- narwal/const.py
+++ narwal/const.py
@@ -2,13 +2,13 @@
 
 import re
 
-__version__ = '0.2.6a'
+__version__ = '0.3.0'
 
 API_PERIOD = 2.0
-BASE_URL = 'http://www.reddit.com'
-LOGIN_URL = 'https://ssl.reddit.com/api/login.json'
-# BASE_URL = 'http://reddit.local:8888'
-# LOGIN_URL = 'http://reddit.local:8888/api/login.json'
+#BASE_URL = 'http://www.reddit.com'
+#LOGIN_URL = 'https://ssl.reddit.com/api/login.json'
+BASE_URL = 'http://reddit.local:8888'
+LOGIN_URL = 'http://reddit.local:8888/api/login.json'
 DEFAULT_USER_AGENT = 'python-narwal/{0}'.format(__version__)
 
 TYPES = {
@@ -22,9 +22,9 @@ TYPES = {
 }
 
 KIND_PATTERN = re.compile(r't(?P<type>[0-9]+)(?:_(?P<id>[a-z0-9]+))?')
-COMMENTS_PATH_PATTERN = re.compile(r'(?P<start>.*comments)/(?P<link_id>\w+)/(?P<link_title>\w+)/.*')
-POST_ERROR_PATTERN = re.compile(r'\[\d+, \d+, "call", \["\.(error\.[^"]+?)"\]\]')
-SUBMIT_RESPONSE_LINK_PATTERN = re.compile(r'\["({0}/.+?)"\]'.format(BASE_URL))
+# COMMENTS_PATH_PATTERN = re.compile(r'(?P<start>.*comments)/(?P<link_id>\w+)/(?P<link_title>\w+)/.*')
+# POST_ERROR_PATTERN = re.compile(r'\[\d+, \d+, "call", \["\.(error\.[^"]+?)"\]\]')
+# SUBMIT_RESPONSE_LINK_PATTERN = re.compile(r'\["({0}/.+?)"\]'.format(BASE_URL))
 
 MAX_REPRSTR = 24
 
